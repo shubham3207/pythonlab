68e9e53061c027994da478aaecfce36daf0e7d40
Saurav <shubham.sapkota1@gmail.com>
2021-02-23 11:14:12 +0530

    pycharm project

---
+++ shubham.py
@@ -0,0 +1,9 @@
+#write a program that takes three numbers and print their sum. every number is given on a separate line
+num1=int(input("enter the first num"))
+num2=int(input("enter the second num"))
+num3=int(input("enter the third num"))
+sum=num1+num2+num3
+print("the sum of given number is",sum)
+
+
+
