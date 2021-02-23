3c7edfeed69c39f7572edef4ed8ecf0c8e766af8
Saurav <shubham.sapkota1@gmail.com>
2021-02-23 11:31:24 +0530

    pycharm 11

---
+++ project1.py
@@ -0,0 +1,11 @@
+#write a program that that reads the base and the height of a right-angled triangle and prints the area.
+#every number is given on a seperate line
+base=int(input(" enter the base"))
+height=int(input("enter the height"))
+area=(base*height)//2
+#the floor division // rounds the result down to the nearest whole number
+print(f"the area of right angled triangle is {area}")
+
+
+
+
